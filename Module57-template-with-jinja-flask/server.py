20fdbe1736be0d81b70e0a1a5035e20a2d4fee96
Vitoria Fortes <fortesvitoria@gmail.com>
2025-10-03 13:32:35 -0300

    add class

--- Module57-template-with-jinja-flask/server.py
+++ Module57-template-with-jinja-flask/server.py
@@ -27,5 +27,12 @@ def guess(name):
 
     return render_template("index.html",name=name, age=age, gender=gender, year=current_year)
 
+@app.route("/blog")
+def get_blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
