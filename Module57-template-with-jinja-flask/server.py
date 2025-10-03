6f1814b9efaaaff6e7c2310eceac6aa2f8798d27
Vitoria Fortes <fortesvitoria@gmail.com>
2025-10-03 10:42:39 -0300

    using api with jinja and flask

--- Module57-template-with-jinja-flask/server.py
+++ Module57-template-with-jinja-flask/server.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template
+# import random
+import datetime
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    # random_number = random.randint(1,10)
+    # current_year= datetime.datetime.now().year
+    return  "Hi, there" #render_template("index.html", num=random_number, year=current_year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data = age_response.json()
+    age = age_data["age"]
+
+    current_year= datetime.datetime.now().year
+
+    return render_template("index.html",name=name, age=age, gender=gender, year=current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
